5b7f782edeba76cf4612f83e4793b59505dfaa49
Hassan <h36ahmed@gmail.com>
2017-01-24 01:58:12 -0500

    Adds Auth in Header

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ var user = require('./user');
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Auth");
     next();
 });
 
