2e817c5b6866b70f7b2ac140607f5279af10432e
Hassan <h36ahmed@gmail.com>
2017-01-28 18:22:34 -0500

    Fixes the relationships of tables

--- server/db.js
+++ server/db.js
@@ -37,21 +37,51 @@ db.Sequelize = Sequelize;
 
 // ASSOCIATIONS
 
+// Users & Owners U:O -> 1:1
 db.owners.belongsTo(db.users);
+
+// Users & Customers U:C -> 1:1
 db.customers.belongsTo(db.users);
+
+// Restaurants & Owners R:O -> 1:1
 db.restaurants.belongsTo(db.owners);
+
+// Referral Codes & Users RC:U -> 1:1
 db.referralCodes.belongsTo(db.users);
 
+// Restaurants & Meals R:M -> 1:M
+db.restaurants.hasMany(db.meals);
+db.meals.belongsTo(db.restaurants);
+
+// Payment Plans & Customers PP:C -> 1:M
 db.paymentPlans.hasMany(db.customers);
+db.customers.belongsTo(db.paymentPlans);
+
+// Customers & Feedbacks C:F -> 1:M
 db.customers.hasMany(db.feedbacks);
-db.orders.hasMany(db.feedbacks, {as: 'order'});
-db.customers.hasMany(db.invoices, {as: 'customer'});
-db.restaurants.hasMany(db.meals, {as: 'restaurant'});
-db.meals.hasMany(db.offers, {as: 'meal'});
-db.offers.hasMany(db.orders, {as: 'offer'});
-db.customers.hasMany(db.orders, {as: 'customer'});
+db.feedbacks.belongsTo(db.customers);
 
+// Orders & Feedbacks O:F -> 1:M
+db.orders.hasMany(db.feedbacks);
+db.feedbacks.belongsTo(db.orders);
 
+// Customers & Invoices C:I -> 1:M
+db.customers.hasMany(db.invoices);
+db.invoices.belongsTo(db.customers);
+
+// Meals & Offers M:O -> 1:M
+db.meals.hasMany(db.offers);
+db.offers.belongsTo(db.meals);
+
+// Offers & Orders Of:Or -> 1:M
+db.offers.hasMany(db.orders);
+db.orders.belongsTo(db.offers);
+
+// Customers & Orders C:O -> 1:M
+db.customers.hasMany(db.orders);
+db.orders.belongsTo(db.customers);
+
+// Customers & Meals ==> Favorites C:M -> M:M
 db.customers.belongsToMany(db.meals, { as: 'Meals', through: db.customerFavorites });
 db.meals.belongsToMany(db.customers, { as: 'Customers', through: db.customerFavorites });
 
