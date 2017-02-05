d7d7c48600dbe1d70356f5f9e982170e04021395
Hassan <h36ahmed@gmail.com>
2017-02-05 18:28:28 -0500

    Fixes the methods

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,6 +19,7 @@ var feedback = require('./feedback');
 
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Auth");
     next();
 });
