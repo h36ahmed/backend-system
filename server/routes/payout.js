f1de4651cd1620b20468bd12687ae1854c2a2e16
Hassan <h36ahmed@gmail.com>
2017-04-04 15:52:54 -0400

    Completes Creating Payout Record

--- server/routes/payout.js
+++ server/routes/payout.js
@@ -29,11 +29,62 @@ exports.list = function(req, res) {
 
 // POST /api/v1/payout
 exports.create = function(req, res) {
-    var payoutDetails = _.pick(req.body, 'payout_date', 'status', 'tax_amount', 'total_payment_before_tax', 'notes', 'restaurant_id', 'total_meals', 'week_id');
-    models.payouts.create(payoutDetails).then(function(payout) {
-        res.json(payout);
+
+    var payoutDetails = _.pick(req.body, 'week_id');
+    var weekID = parseInt(payoutDetails.week_id, 10);
+    var where = {};
+
+    models.weeks.findById(weekID).then(function(week) {
+        var weekDetails = week.toJSON();
+        models.restaurants.findAll({
+            where: where,
+            attributes: ['name'],
+            include: [{
+                model: models.meals,
+                attributes: ['name', 'price'],
+                include: [{
+                    model: models.offers,
+                    attributes: ['plates_assigned', 'plates_left', 'offer_date'],
+                    where: {
+                        offer_date: {
+                            $gt: weekDetails.from_date,
+                            $lte: weekDetails.to_date
+                        }
+                    }
+                }]
+            }]
+        }).then(function(restuarants) {
+            var payouts = [];
+            _.each(restuarants, function(restaurant) {
+                var payout = {
+                    restaurant: '',
+                    total_meals: 0,
+                    total_payment_before_tax: 0,
+                    tax_amount: 0,
+                    total_amount: 0
+                };
+                payout.restaurant = restaurant.name;
+                _.each(restaurant.meals, function(meal) {
+                    _.each(meal.offers, function(offer) {
+                        var totalPlatesServed = offer.plates_assigned - offer.plates_left;
+                        payout.total_payment_before_tax += totalPlatesServed * meal.price;
+                        payout.total_meals += totalPlatesServed;
+                    });
+                });
+                payout.tax_amount = payout.total_payment_before_tax * 0.13;
+                payout.total_amount = payout.total_payment_before_tax + payout.tax_amount;
+                payouts.push(payout);
+            });
+            models.payouts.bulkCreate(payouts).then(function(bulk) {
+                res.status(204).send();
+            }, function(err) {
+                res.status(500).send();
+            });
+        }, function(e) {
+            res.status(500).send();
+        });
     }, function(e) {
-        res.status(400).json(e);
+        res.status(404).json(e);
     });
 };
 
