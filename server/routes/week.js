2a9c97bc54384d7c71cfa6ca753c8887f39e3d0d
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-06-02 14:31:57 -0400

    Added conditional statement for meal offers route

--- server/routes/week.js
+++ server/routes/week.js
@@ -53,35 +53,10 @@ exports.list = function(req, res) {
     var query = req.query;
     var where = {};
 
-    req.query.hasOwnProperty('id') ? where.id = req.query.id : null
-
     models.weeks.findAll({
         where: where
     }).then(function(weeks) {
-        const weekDetails = weeks[0].toJSON()
-        models.restaurants.findAll({
-            // where: where,
-            attributes: ['name'],
-            include: [{
-                model: models.meals,
-                attributes: ['name', 'price'],
-                include: [{
-                    model: models.offers,
-                    attributes: ['plates_assigned', 'plates_left', 'offer_date'],
-                    where: {
-                        offer_date: {
-                            $gte: formatNumberDate(weekDetails.from_date),
-                            $lte: formatNumberDate(weekDetails.to_date)
-                        }
-                    }
-                }]
-            }]
-        })
-        .then(restaurants => {
-
-        res.json(restaurants);
-
-        })
+        res.json(weeks)
     }, function(e) {
         res.status(500).send();
     });
@@ -93,52 +68,75 @@ exports.view = function(req, res) {
     var type = req.query.type
     var where = {};
 
+    req.query.restaurantID ? where.id = req.query.restaurantID : null
+
     models.weeks.findById(weekID).then(function(week) {
         var weekDetails = week.toJSON();
-        models.restaurants.findAll({
-            where: where,
-            attributes: ['name'],
-            include: [{
-                model: models.meals,
-                attributes: ['name', 'price'],
-                include: [{
-                    model: models.offers,
-                    attributes: ['plates_assigned', 'plates_left', 'offer_date'],
-                    where: {
-                        offer_date: {
-                            $gte: weekDetails.from_date,
-                            $lte: weekDetails.to_date
-                        }
-                    }
-                }]
-            }]
-        }).then(function(restuarants) {
 
-                var payouts = [];
-                _.each(restuarants, function(restaurant) {
-                    var payout = {
-                        restaurant: '',
-                        total_meals: 0,
-                        total_payment_before_tax: 0,
-                        tax_amount: 0,
-                        total_amount: 0
-                    };
-                    payout.restaurant = restaurant.name;
-                    _.each(restaurant.meals, function(meal) {
-                        _.each(meal.offers, function(offer) {
-                            var totalPlatesServed = offer.plates_assigned - offer.plates_left;
-                            payout.total_payment_before_tax += totalPlatesServed * meal.price;
-                            payout.total_meals += totalPlatesServed;
+        if (type === 'resMealOffer') {
+            models.offers.findAll({
+                where: {
+                    offer_date: {
+                        $gte: formatNumberDate(weekDetails.from_date),
+                        $lte: formatNumberDate(weekDetails.to_date)
+                    }
+                },
+                include: [{
+                    model: models.meals,
+                    attributes: ['name', 'ingredients']
+                }]
+            })
+            .then(offers => {
+                res.json(offers)
+            }, e => {
+                res.status(500).send()
+            })
+        } else {
+            models.restaurants.findAll({
+                where: where,
+                attributes: ['name'],
+                include: [{
+                    model: models.meals,
+                    attributes: ['name', 'price'],
+                    include: [{
+                        model: models.offers,
+                        attributes: ['plates_assigned', 'plates_left', 'offer_date'],
+                        where: {
+                            offer_date: {
+                                $gte: weekDetails.from_date,
+                                $lte: weekDetails.to_date
+                            }
+                        }
+                    }]
+                }]
+            })
+            .then(function(restuarants) {
+                    var payouts = [];
+                    _.each(restuarants, function(restaurant) {
+                        var payout = {
+                            restaurant: '',
+                            total_meals: 0,
+                            total_payment_before_tax: 0,
+                            tax_amount: 0,
+                            total_amount: 0
+                        };
+                        payout.restaurant = restaurant.name;
+                        _.each(restaurant.meals, function(meal) {
+                            _.each(meal.offers, function(offer) {
+                                var totalPlatesServed = offer.plates_assigned - offer.plates_left;
+                                payout.total_payment_before_tax += totalPlatesServed * meal.price;
+                                payout.total_meals += totalPlatesServed;
+                            });
                         });
+                        payout.tax_amount = payout.total_payment_before_tax * 0.13;
+                        payout.total_amount = payout.total_payment_before_tax + payout.tax_amount;
+                        payouts.push(payout);
                     });
-                    payout.tax_amount = payout.total_payment_before_tax * 0.13;
-                    payout.total_amount = payout.total_payment_before_tax + payout.tax_amount;
-                    payouts.push(payout);
-                });
-                type === 'resMealOffer' ? res.json(restuarants) : res.json(payouts)
-        }, function(e) {
-            res.status(500).send();
-        });
+                    res.json(payouts)
+            }, function(e) {
+                res.status(500).send();
+            });
+        }
     }, function(e) {
         res.status(404).json(e);
     });
