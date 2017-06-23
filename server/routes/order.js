de9330db37fddf6472b511b64c5f919fb9a737c4
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-06-23 14:52:55 -0400

    Added moment package to orders

--- server/routes/order.js
+++ server/routes/order.js
@@ -4,6 +4,7 @@ const email = require('./email')
 const payment = require('./payment.js')
 const ics = require('../ics-generator.js')
 const fs = require('fs')
+const moment = require('moment')
 
 
 // GET /api/v1/orders
@@ -94,7 +95,7 @@ exports.create = function(req, res) {
       .then(order => {
         emailData.date = order.order_date
         emailData.ICSDate = order.order_date
-        // console.log('emaildata', moment(emailData.ICSDate)
+        console.log('emaildata', moment(emailData.ICSDate)
 
         models.offers.findById(order.offer_id, {
           include: [{
