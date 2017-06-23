d394c6a04e2620dfc0f746677fb5d6625c8fd30b
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-06-23 13:40:16 -0400

    Added a string to the console.log()

--- server/routes/order.js
+++ server/routes/order.js
@@ -94,7 +94,7 @@ exports.create = function(req, res) {
       .then(order => {
         emailData.date = order.order_date
         emailData.ICSDate = order.order_date
-        console.log(emailData.ICSDate)
+        console.log('emaildata', emailData.ICSDate)
 
         models.offers.findById(order.offer_id, {
           include: [{
