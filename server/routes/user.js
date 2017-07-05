e2dbfb40c81372971c5cfec594a62f041abc94f3
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-05 18:55:08 -0400

    Changed parseZone back to format

--- server/routes/user.js
+++ server/routes/user.js
@@ -149,10 +149,10 @@ exports.login = function(req, res) {
                     }})
                     .then(order => {
                         if (order) {
-                            const today = moment().parseZone('YYYY-MM-DD')
+                            const today = moment().format('YYYY-MM-DD')
                             //moment rounds down when date has T00:00:00.000Z
                             //e.g 2017-07-05T00:00:00.000Z with moment === 2017-07-04
-                            const tomorrowOrderDate = moment(order.order_date).add(1, 'd').parseZone('YYYY-MM-DD')
+                            const tomorrowOrderDate = moment(order.order_date).add(1, 'd').format('YYYY-MM-DD')
 
                             console.log('order', order.toJSON())
                             console.log('today', today)
