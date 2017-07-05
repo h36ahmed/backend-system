1bb20f8cf59eaba2e9445a74f4cc285adc224413
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-05 18:43:47 -0400

    More console.log()

--- server/routes/user.js
+++ server/routes/user.js
@@ -148,14 +148,15 @@ exports.login = function(req, res) {
                         customer_id: customer[0].id
                     }})
                     .then(order => {
-                        console.log('order.toJSON()', order.toJSON())
                         if (order) {
                             const today = moment().format('YYYY-MM-DD')
                             const tomorrowOrderDate = moment(order.order_date).add(1, 'd').format('YYYY-MM-DD')
 
+                            console.log('test' moment(order.order_date).format('YYYY-MM-DD'))
                             console.log('order', order.toJSON())
                             console.log('today', today)
                             console.log('tomorrowOrderDate', tomorrowOrderDate)
+
 
                             if (tomorrowOrderDate === today) {
                                 userSend.needOrderFeedback = order.toJSON().id
