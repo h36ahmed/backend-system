eb96fc3d2ecdd640591dce872f0980cbcf9cb3c5
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-26 21:52:50 -0400

    Changed email

--- server/routes/email.js
+++ server/routes/email.js
@@ -80,7 +80,7 @@ var sendCustomerWelcomeEmail = function (data, res) {
         from: from_who,
         data: data,
         templateID: 2066904,
-        bcc: 'admin@lunchsociety.ca'
+        bcc: 'jeffreycj.chang@gmail.com'
     }
 
     response = res;
@@ -109,7 +109,7 @@ var sendOrderEmail = function (data, res) {
         data: data,
         templateID: 2067882,
         ics: true,
-        bcc: 'admin@lunchsociety.ca'
+        bcc: 'jeffreycj.chang@gmail.com'
     }
 
     response = res;
