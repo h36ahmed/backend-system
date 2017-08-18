81644493a9d919e728471e4d937ea48528bd386a
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-08-17 20:47:08 -0400

    Added .send() to the status

--- server/routes/email.js
+++ server/routes/email.js
@@ -243,7 +243,7 @@ exports.sendROEmail = function (req, res) {
                     // Throwing inside a promise will just reject the promise
                     // not stop your server
                     if (err) throw err
-                    res.status(200)
+                    res.status(200).send()
                     console.info('Messages sent to postmark');
                 });
             });
